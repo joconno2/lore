f4b3a00a399d18b3de61c51936fe62158cce920e
Jim O'Connor <jim@oconnor-computing.com>
2026-05-31 03:15:22 -0400

    Fix NetHackChallenge-v0 env: remove duplicate actions param

--- scripts/eval_expert.py
+++ scripts/eval_expert.py
@@ -30,9 +30,9 @@ def make_env(seed=None, character="val-hum-fem-neu"):
             "glyphs", "blstats", "message", "misc",
             "inv_glyphs", "inv_strs", "inv_letters", "inv_oclasses",
         ),
-        actions=nethack.ACTIONS,
         character=character,
         max_episode_steps=5000,
+        no_progress_timeout=500,
     )
     return env
 
